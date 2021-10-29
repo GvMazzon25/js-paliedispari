390b7dad126b8aad97d119ac0e5f799ddc655304
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-29 15:23:09 +0200

    end-project-pali

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -7,13 +7,29 @@
 
 //Variabile
 
-const word = prompt('Scrivi una parola');
-console.log(word);
+let userWord = prompt('Scrivi una parola');
+console.log(userWord);
 
 
 
+const wordReverse = wordPali (userWord);
+console.log (wordReverse);
 
-
+if(wordReverse === userWord){
+    alert(`La parola ${userWord} è palindroma`);
+}else{
+    alert (`La parola ${userWord} non è palindroma`);
+}
 
 
 //Funzioni
+
+function wordPali (userWord){
+    let reverse = '';
+    for(let i = userWord.length -1; i >= 0; i--){
+        console.log(userWord[i]);
+        reverse += userWord[i];
+    }
+
+    return reverse; 
+}
