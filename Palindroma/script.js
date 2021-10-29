578a4a8284dfecbae2406d162e8065c5ec043a32
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-29 14:40:50 +0200

    start-project-pali

---
+++ Palindroma/script.js
@@ -0,0 +1,19 @@
+//Parola Palindroma
+
+/**
+ * Chiedere all’utente di inserire una parola
+   Creare una funzione per capire se la parola inserita è palindroma
+ */
+
+//Variabile
+
+const word = prompt('Scrivi una parola');
+console.log(word);
+
+
+
+
+
+
+
+//Funzioni
